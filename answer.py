b368f109a632b464dfb5e1165a4b8287ecddaec4
vvunnam68 <vvunnam@stevens.edu>
2024-01-24 00:14:14 -0500

    Homewoek 0

--- answer.py
+++ answer.py
@@ -2,16 +2,21 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
+    This function prints the "Hello, World!" statement
 
     """
 
-    # Write a comment; comments are very useful to other programmers.
-
     # Write a Python statement to print Hello, World!
+    print("Hello, World!")
 
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    """
+    This function returns the addition of two numbers
 
+    """
+    output = a+b
+    
     return output
+    
